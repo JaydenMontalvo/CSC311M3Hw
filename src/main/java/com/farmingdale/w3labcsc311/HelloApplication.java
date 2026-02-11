36cb36bad4f7057729ed3ad11cc62b7db8c5773b
JaydenMontalvo <Jtmontalvo05@gmail.com>
2026-02-11 13:18:49 -0500

    Css Implementation

--- src/main/java/com/farmingdale/w3labcsc311/HelloApplication.java
+++ src/main/java/com/farmingdale/w3labcsc311/HelloApplication.java
@@ -21,48 +21,76 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setScene(scene);
-        stage.setTitle("Loan Calculator");
-        stage.show();
-//    GridPane gridpane =  new GridPane();
-//    gridpane.setId("Loan Calculator");
-//    gridpane.setPadding(new Insets(10, 10, 10, 20));
-//    gridpane.setVgap(5);
-//        //Annual Interest Rate
-//        Label label1 = new Label("Annual Interest Rate: ");
-//        GridPane.setConstraints(label1, 0, 0);
-//        TextField AIR = new TextField();
-//        GridPane.setConstraints(AIR, 1, 0);
-//        //Number Of Years
-//        Label label2 = new Label("Number Of Years: ");
-//        GridPane.setConstraints(label2, 0, 1);
-//        TextField NOY = new TextField();
-//        GridPane.setConstraints(NOY, 1, 1);
-//        //Loan Amount
-//        Label label3 = new Label("Loan Amount: ");
-//        GridPane.setConstraints(label3, 0, 2);
-//        TextField LA = new TextField();
-//        GridPane.setConstraints(LA, 1, 2);
-//        //Monthly Payment
-//        Label label4 = new Label("Monthly Payment: ");
-//        GridPane.setConstraints(label4, 0, 3);
-//        TextField MP = new TextField();
-//        GridPane.setConstraints(MP, 1, 3);
-//        //Total Payment
-//        Label label5 = new Label("Total Payment: ");
-//        GridPane.setConstraints(label5, 0, 4);
-//        TextField TP = new TextField();
-//        GridPane.setConstraints(TP, 1, 4);
-//        //Button
-//        Button btn = new Button("Calculate");
-//        GridPane.setConstraints(btn, 1, 5);
-//        GridPane.setHalignment(btn, HPos.RIGHT);
-//        gridpane.getChildren().addAll(label1, AIR, label2, NOY, label3, LA, label4, MP, label5, TP, btn);
-//        Scene scene = new Scene(gridpane, 300, 200);
+        //FXML
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+//        Scene scene = new Scene(loader.load());
 //        stage.setScene(scene);
+//        stage.setTitle("Loan Calculator");
 //        stage.show();
+//
+        //Java
+
+    GridPane gridpane =  new GridPane();
+    gridpane.setId("Loan Calculator");
+    gridpane.setPadding(new Insets(10, 10, 10, 20));
+    gridpane.setVgap(5);
+        //Annual Interest Rate
+        Label label1 = new Label("Annual Interest Rate: ");
+        GridPane.setConstraints(label1, 0, 0);
+        TextField AIR = new TextField();
+        GridPane.setConstraints(AIR, 1, 0);
+        //Number Of Years
+        Label label2 = new Label("Number Of Years: ");
+        GridPane.setConstraints(label2, 0, 1);
+        TextField NOY = new TextField();
+        GridPane.setConstraints(NOY, 1, 1);
+        //Loan Amount
+        Label label3 = new Label("Loan Amount: ");
+        GridPane.setConstraints(label3, 0, 2);
+        TextField LA = new TextField();
+        GridPane.setConstraints(LA, 1, 2);
+        //Monthly Payment
+        Label label4 = new Label("Monthly Payment: ");
+        GridPane.setConstraints(label4, 0, 3);
+        TextField MP = new TextField();
+        GridPane.setConstraints(MP, 1, 3);
+        //Total Payment
+        Label label5 = new Label("Total Payment: ");
+        GridPane.setConstraints(label5, 0, 4);
+        TextField TP = new TextField();
+        GridPane.setConstraints(TP, 1, 4);
+        //Button
+        Button btn = new Button("Calculate");
+        btn.setOnAction(e -> {
+            try {
+                double annualInterestRate = Double.parseDouble(AIR.getText());
+                int numberOfYears = Integer.parseInt(NOY.getText());
+                double loanAmount = Double.parseDouble(LA.getText());
+
+                double monthlyInterestRate = annualInterestRate / 1200;
+                double monthlyPayment = loanAmount * monthlyInterestRate /
+                        (1 - Math.pow(1 + monthlyInterestRate, -numberOfYears * 12));
+                double totalPayment = monthlyPayment * numberOfYears * 12;
+
+
+                MP.setText(String.format("$" + "%.2f" , monthlyPayment));
+                TP.setText(String.format("$" + "%.2f", totalPayment));
+            } catch (NumberFormatException ex) {
+
+                MP.setText("Invalid input");
+                TP.setText("Invalid input");
+            }
+        });
+
+
+        GridPane.setConstraints(btn, 1, 5);
+        GridPane.setHalignment(btn, HPos.RIGHT);
+        gridpane.getChildren().addAll(label1, AIR, label2, NOY, label3, LA, label4, MP, label5, TP, btn);
+        Scene scene = new Scene(gridpane, 350, 200);
+        scene.getStylesheets().add(getClass().getResource("/myStyle.css").toExternalForm());
+        stage.setTitle("Loan Calculator");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
